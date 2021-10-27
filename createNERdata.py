f1fe10b536f5dabca2192035432ce1f20de4296c
pranav-thampi-numerator <74964889+pranav-thampi-numerator@users.noreply.github.com>
2021-10-27 18:37:09 +0530

    added summary extraction

--- createNERdata.py
+++ createNERdata.py
@@ -12,20 +12,26 @@ class createNERdata():
 
     @Language.component('clean_data_matcher')
     def clean_data_matcher(self,entity_label, doc):
-        patterns = {"item_rsd": r'(?:Price Qty Total\s)?([\w\s\-\+\(\#\)\$]{5,64})\s\$',
-                    "item_qty_amount": r'(\d{1,3})\s\$(\d{1,3}\.\d{2})'
+        patterns = {"item_rsd": r'(?:Price Qty Total\s)?([\w\s\-\+\(\)\$\#]{5,64})\s\$',
+                    "item_qty_amount": r'(\d{1,3})\s\$(\d{1,3}\.\d{2})',
+                    "subtotal": r"Subtotal\:\s\$(\d{1,3}\.\d{2})",
+                    "tax": r"Tax\:\s\$(\d{1,3}\.\d{2})",
+                    "total": r"Total\:\s\$(\d{1,3}\.\d{2})"
                     }
         for pattern, expression in patterns.items():
             if entity_label == pattern:
                 result = re.findall(expression, doc)
-                print("result",result)
+                # print("result",result)
         return result if result else None
 
-    @Language.component('item_regex_matcher')
-    def item_regex_matcher(doc):
+    @Language.component('regex_matcher')
+    def regex_matcher(doc):
         expressions = {
+            "item_rsd": re.compile(r"([\w\s\-\+\(\#\)\$]{5,64})\s\$\d{1,3}\.\d{2}"),
             "item_qty_amount": re.compile(r"(\d{1,3})\s\$(\d{1,3}\.\d{2})"),
-            "item_rsd": re.compile(r"([\w\s\-\+\(\#\)\$]{5,64})\s\$\d{1,3}\.\d{2}")
+            "subtotal": re.compile(r"Subtotal\:\s\$(\d{1,3}\.\d{2})"),
+            "tax": re.compile(r"Tax\:\s\$(\d{1,3}\.\d{2})"),
+            "total": re.compile(r"Total\:\s\$(\d{1,3}\.\d{2})")
         }
         spans = []
         for labels, expression in expressions.items():
@@ -38,56 +44,105 @@ class createNERdata():
         return doc
 
 
-    def create_item_entity(self,item_text):
+    def create_entity(self,item_text):
         nlp = spacy.load("en_core_web_lg", disable=['ner'])
-        nlp.add_pipe('item_regex_matcher')
+        nlp.add_pipe('regex_matcher')
         doc = nlp(item_text)
         return doc
 
     def load_entity(self):
         receipt_no, item_texts, summary_texts = GetReceiptText().getText()
 
+        for i in range(len(item_texts)):
+            doc=self.create_entity(item_texts[i])
+            doc2=self.create_entity(summary_texts[i])
+            # db = DocBin()
+            # db.add(doc)
+            # db.to_disk(
+            #     "/Users/poonam.yadav/Desktop/FallHackday2021_Project/Fall_Hack_day_2021/data/training_data/train.spacy")
+            print("************************Receipt id-{0}********************".format(receipt_no[i]))
+            print("--------------entities----------------")
+            self.itemExtracter(doc, receipt_no[i])
+            self.summaryExtracter(doc2, receipt_no[i])
+
+    def itemExtracter(self, doc, receipt_no):
         receipt_num = []
         item_rsd = []
         item_qty = []
         item_price = []
-        for i in range(len(item_texts)):
-            doc=self.create_item_entity(item_texts[i])
-            db = DocBin()
-            db.add(doc)
-            db.to_disk("/Users/poonam.yadav/Desktop/FallHackday2021_Project/Fall_Hack_day_2021/data/training_data/train.spacy")
-            print("************************Receipt id-{0}********************".format(receipt_no[i]))
-            print("--------------entities----------------")
-            for ent in doc.ents:
-                # print("entity is {0} and label is {1}".format(ent, ent.label_))
-                if ent.label_ == "item_rsd":
-                    abc = self.clean_data_matcher(ent.label_,ent.text)
-                    # print("ent", abc)
-                    print("\nentity is:item name-{0}".format(abc))
-                    receipt_num.append(receipt_no[i])
-                    item_rsd.append(abc[0])
-                else:
-                    abc = self.clean_data_matcher(ent.label_,ent.text)
-                    # print("ent", abc)
-                    item_qty.append(abc[0][0])
-                    item_price.append(abc[0][1])
-                    try:
-                        print("entity is:qty-{0} amount-{1}".format(abc[0][0], abc[0][1]))
-                    except:
-                        print("Failed")
+
+        for ent in doc.ents:
+            # print("entity is {0} and label is {1}".format(ent, ent.label_))
+            if ent.label_ == "item_rsd":
+                abc = self.clean_data_matcher(ent.label_, ent.text)
+                # print("ent", abc)
+                print("\nentity is:item name-{0}".format(abc))
+                receipt_num.append(receipt_no)
+                item_rsd.append(abc[0])
+            elif ent.label_ == "item_qty_amount":
+                abc = self.clean_data_matcher(ent.label_, ent.text)
+                # print("ent", abc)
+                item_qty.append(abc[0][0])
+                item_price.append(abc[0][1])
+                try:
+                    print("entity is:qty-{0} amount-{1}".format(abc[0][0], abc[0][1]))
+                except:
+                    print("Failed")
+
         data = {
             'Receipt': receipt_num,
             'Item_RSD': item_rsd,
             'Item_Qty': item_qty,
             'Item_Price': item_price
         }
-        print(data)
-        print("receipt_num:", len(receipt_num))
-        print("item_rsd:", len(item_rsd))
-        print("item_qty:", len(item_qty))
-        print("item_price:", len(item_price))
         df = pd.DataFrame(data)
-        df.to_csv("values.csv", mode="w", index=False)
+        df.to_csv("items.csv", mode="w", index=False)
+
+    def summaryExtracter(self, doc, receipt_no):
+        receipt_num = []
+        subtotal = []
+        tax = []
+        total = []
+
+        receipt_num.append(receipt_no)
+        for ent in doc.ents:
+            # print("entity is {0} and label is {1}".format(ent, ent.label_))
+            if ent.label_ == "subtotal":
+                abc = self.clean_data_matcher(ent.label_, ent.text)
+                # print("ent", abc)
+                print("subtotal", abc[0])
+                subtotal.append(abc[0])
+                # try:
+                #     print("entity is:qty-{0} amount-{1}".format(abc[0][0], abc[0][1]))
+                # except:
+                #     print("Failed")
+            elif ent.label_ == "tax":
+                abc = self.clean_data_matcher(ent.label_, ent.text)
+                # print("ent", abc)
+                print("tax", abc[0])
+                tax.append(abc[0])
+                # try:
+                #     print("entity is:qty-{0} amount-{1}".format(abc[0][0], abc[0][1]))
+                # except:
+                #     print("Failed")
+            elif ent.label_ == "total":
+                abc = self.clean_data_matcher(ent.label_, ent.text)
+                # print("ent", abc)
+                print("total", abc[0])
+                total.append(abc[0])
+                # try:
+                #     print("entity is:qty-{0} amount-{1}".format(abc[0][0], abc[0][1]))
+                # except:
+                #     print("Failed")
+
+        data = {
+            'Receipt': receipt_num,
+            'Subtotal': subtotal,
+            'Tax': tax,
+            'Total': total
+        }
+        df = pd.DataFrame(data)
+        df.to_csv("summary.csv", mode="w", index=False)
 
 if __name__ == "__main__":
     createNERdata().load_entity()
